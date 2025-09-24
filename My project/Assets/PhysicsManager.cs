2f5ada7fda4480708a786196b7730c7d6e11db61
Rob Sheehy <robert.sheehy@ittralee.ie>
2025-09-24 10:30:31 +0100

    Update PhysicsManager.cs

--- My project/Assets/PhysicsManager.cs
+++ My project/Assets/PhysicsManager.cs
@@ -7,16 +7,37 @@ using UnityEngine;
 public class PhysicsManager : MonoBehaviour
 {
     List<sphereScript> spheres;
+
+    //List<IPhysical> allObjects;
+    
     // List<PLanescripts> planes;?????
     // Start is called before the first frame update
     void Start()
     {
         spheres = FindObjectsOfType<sphereScript>().ToList();
+        allObjects = FindObjectsOfType<IPhysical>().ToList();  // Note we wish to find all IMPLEMENTATIONS of this interface
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        for (int i = 0; i < allObjects.Count-1; i++)
+        {
+            for (int j = i+1;  j < allObjects.Count; j++)
+            {
+                if (allObjects[i].isColliding( allObjects[j]))
+                {
+                    allObjects[j].velocity =
+                        allObjects[i].resolvedVolocityforCollisionWith(allObjects[j]);
+                 // (Vector3 , Vector3) (position, velocity) = 
+                //  allObjects[i].resolvedVolocityforCollisionWith(allObjects[j]);
+
+                }
+            }
+
+    
         for (int i = 0; i < spheres.Count-1; i++)
         {
             for (int j = i+1;  j < spheres.Count; j++)
@@ -25,7 +46,8 @@ public class PhysicsManager : MonoBehaviour
                 {
                     spheres[j].velocity =
                         spheres[i].resolvedVolocityforCollisionWith(spheres[j]);
-              
+                 // (Vector3 , Vector3) (position, velocity) = 
+                //  spheres[i].resolvedVolocityforCollisionWith(spheres[j]);
 
                 }
             }
